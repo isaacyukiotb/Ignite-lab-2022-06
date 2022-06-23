254c35e236328e01a87ea05a2b4015b0b2604485
Isaac Yukio <isaacyukio@gmail.com>
2022-06-23 18:33:46 -0300

    Instalando as depedencias de rotas, criando as routes da aplicacao, instalando a bilbioteca de video, adicionando video na nossa aplicacao

--- event-platform/src/components/Lesson.tsx
+++ event-platform/src/components/Lesson.tsx
@@ -1,6 +1,8 @@
 import { CheckCircle, Lock } from 'phosphor-react'
 import { isPast, format} from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
+import { Link } from 'react-router-dom';
+
 interface LessonProps {
     title: string;
     slug: string;
@@ -9,18 +11,20 @@ interface LessonProps {
 }
 
 export function Lesson({title, slug, availableAt, type, ...props }: LessonProps) {
+
     const isLessonAvailable = isPast(availableAt);
     const availableDateFormatted = format(availableAt, "EEEE' • 'd' de 'MMMM' • 'k'h'mm",{
         locale:ptBR
     })
+    
 
     return (
-        <a href="#">
+        <Link to={`/event/lesson/${slug}`} className='group'>
             <span className="text-gray-300">
                 {availableDateFormatted}
             </span>
 
-            <div className="rounded border border-gray-500 p-4 mt-2">
+            <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
 
                 <header className="flex items-center justify-between">
                     {
@@ -45,6 +49,6 @@ export function Lesson({title, slug, availableAt, type, ...props }: LessonProps)
                     {title}
                 </strong>
             </div>
-        </a>
+        </Link>
     )
 }
