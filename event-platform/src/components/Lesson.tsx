0c02264ebd9121641f5ccba34365e0ae0253004f
Isaac Yukio <isaacyukio@gmail.com>
2022-06-24 18:33:52 -0300

    Deixando o as aulas em breve desabilitadas

--- event-platform/src/components/Lesson.tsx
+++ event-platform/src/components/Lesson.tsx
@@ -12,7 +12,7 @@ interface LessonProps {
 }
 
 export function Lesson(props: LessonProps) {
-    
+
     const { slug } = useParams<{ slug: string }>();
 
     const isLessonAvailable = isPast(props.availableAt);
@@ -23,24 +23,30 @@ export function Lesson(props: LessonProps) {
     const isActiveLesson = props.slug === slug;
 
     return (
-        <Link to={`/event/lesson/${props.slug}`} onClick={window.location.reload} className='group'>
+        <Link
+            to={isLessonAvailable?`/event/lesson/${props.slug}`:"#"}
+            onClick={isLessonAvailable ? window.location.reload : undefined} 
+            className='group'>
             <span className="text-gray-300">
                 {availableDateFormatted}
             </span>
 
 
             <div
-                className={classNames("rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500", {
+                className={classNames("rounded border border-gray-500 p-4 mt-2 ", {
                     'bg-green-500': isActiveLesson,
+                    'group-hover:border-green-500': isLessonAvailable,
+                    'bg-gray-600': !isLessonAvailable,
+
                 })}
             >
 
                 <header className="flex items-center justify-between">
                     {
                         isLessonAvailable ? (
-                            <span className={classNames('text-sm font-medium flex items-center gap-2',{
-                                'text-white':isActiveLesson,
-                                'text-blue-500':!isActiveLesson,
+                            <span className={classNames('text-sm font-medium flex items-center gap-2', {
+                                'text-white': isActiveLesson,
+                                'text-blue-500': !isActiveLesson,
                             })}>
                                 <CheckCircle size={20} />
                                 Conteúdo liberado
@@ -52,9 +58,9 @@ export function Lesson(props: LessonProps) {
                             </span>
                         )
                     }
-                    <span className={classNames("text-xs rounded px-2 py-[0.125rem] text-white border  font-bold",{
-                        'border-white':isActiveLesson,
-                        'border-green-300':!isActiveLesson,
+                    <span className={classNames("text-xs rounded px-2 py-[0.125rem] text-white border  font-bold", {
+                        'border-white': isActiveLesson,
+                        'border-green-300': !isActiveLesson,
                     })}>
                         {props.type === 'live' ? 'AO VIVO' : 'AULA PRATICA'}
                     </span>
